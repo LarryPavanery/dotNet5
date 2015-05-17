4aef5bc86a3f949743743a7986fe406994c24bd3
Larry Pavanery <larry_expert@hotmail.com>
2015-05-17 15:16:15 -0300

    alterando estrutura de dados de array para list

--- console/Program.cs
+++ console/Program.cs
@@ -1,17 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace console
 {
     public class Program
-    {
-        public static Product[] products = new Product[10];
-        
+    { 
+        public static List<Product> products = new List<Product>();
         public static void Main(string[] args)
         {
             for (var i = 0; i < 10; i++)
             {
                 Product p = new Product { Name = "Car-" + i, Price = 20000 + (i * 10) };
-                products[i] = p;
+                products.Add(p);
                 Console.WriteLine("Create product: " + p.ToString());
             }
             
